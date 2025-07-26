3e52bf5a54e6d2504873a3e16209369941be301a
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-07-26 22:06:16 +0530

    created specific userProfile routee

---
+++ src/app/api/users/[id]/route.js
@@ -0,0 +1,22 @@
+// app/api/users/[id]/route.js
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextResponse } from "next/server";
+
+export async function GET(req, context) {
+  await connect(); // Ensure DB is connected before query
+  const userId = context.params.id;
+
+  try {
+    const user = await User.findById(userId).select("-password -__v");
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ data: user }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}

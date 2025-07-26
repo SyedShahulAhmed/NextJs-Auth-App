28f7b22cc68cc07a563c281cafb69a3bcd97c854
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-07-27 00:04:53 +0530

    created User route

---
+++ src/app/api/users/user/route.js
@@ -0,0 +1,22 @@
+
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextResponse } from "next/server";
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel.js";
+
+connect();
+
+export async function GET(request) {
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({ _id: userId }).select("-password");
+        
+        return NextResponse.json({
+            message : "User data fetched successfully",
+            data : user,
+        });
+    } catch (error) {
+        console.error("Error fetching user data:", error); // avoid toast here, this is a server route
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+}
